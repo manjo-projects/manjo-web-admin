396adefc2188196ef16a768e4bb546b7067e858d
Ivan Hontarenko <dezz@macbookPro.local>
2019-10-13 20:07:53 +0300

    upd_mac

---
+++ adminfly/modules/key-vault/src/main/java/org/papaja/adminfly/module/kv/mapper/KVMapper.java
@@ -0,0 +1,22 @@
+package org.papaja.adminfly.module.kv.mapper;
+
+import org.papaja.adminfly.commons.mapper.AbstractMapper;
+import org.papaja.adminfly.module.kv.dto.KeyValueDto;
+import org.papaja.adminfly.module.kv.entity.KeyValue;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KVMapper extends AbstractMapper<KeyValueDto, KeyValue> {
+
+    @Override
+    public void accept(KeyValueDto source, KeyValue target) {
+        target.setKey(source.getKey().toUpperCase());
+        target.setValue(source.getValue());
+    }
+
+    @Override
+    public KeyValue get() {
+        return new KeyValue();
+    }
+
+}
