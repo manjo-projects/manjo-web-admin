78fcdca8b3c14cd96c8e5aac496a329a0325dfbd
Ivan <ivan.gontarenko@gmail.com>
2019-11-06 17:40:31 +0200

    upd

--- adminfly/build/src/main/java/org/papaja/adminfly/config/initializer/SharedApplicationInitializer.java
+++ adminfly/build/src/main/java/org/papaja/adminfly/config/initializer/SharedApplicationInitializer.java
@@ -1,26 +1,36 @@
 package org.papaja.adminfly.config.initializer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
 
 import java.util.Random;
 
 public class SharedApplicationInitializer
-        extends AbstractAnnotationConfigDispatcherServletInitializer {
+        extends AbstractDispatcherServletInitializer {
 
     @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[] {};
+    protected WebApplicationContext createServletApplicationContext() {
+        AnnotationConfigWebApplicationContext normalWebAppContext = new AnnotationConfigWebApplicationContext();
+        normalWebAppContext.register(SharedWebConfiguration.class);
+        return normalWebAppContext;
     }
 
     @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class[] {SharedWebConfiguration.class};
+    protected WebApplicationContext createRootApplicationContext() {
+        return null;
     }
 
     @Override
@@ -33,23 +43,60 @@ public class SharedApplicationInitializer
         return "sharedServlet";
     }
 
-    @ComponentScan(
-            basePackages = {
-                    "org.papaja.adminfly.config.initializer.web"
-            }
-    )
-    @Configuration
-    public class SharedWebConfiguration implements WebMvcConfigurer {
+    public static class SharedWebConfiguration implements WebMvcConfigurer {
 
-//        @Bean
-//        @Scope(value = WebApplicationContext.SCOPE_REQUEST)
-//        public Integer randomInt() {
-//            System.out.println(getClass().getName());
-//            return new Random().nextInt();
-//        }
+        @Bean
+        @Scope(value = WebApplicationContext.SCOPE_REQUEST)
+        public Integer randomInt() {
+            System.out.println(getClass().getName());
+            return new Random().nextInt();
+        }
 
     }
 
+    @Controller("sharedServletIndexController")
+    public static class SharedController {
+
+        @Autowired
+        private ApplicationContext context;
+
+        @Autowired
+        @Qualifier("mvcViewResolver")
+        private ViewResolver resolver;
+
+        @Autowired
+        @Qualifier("viewResolver")
+        private ViewResolver resolver2;
+
+        public SharedController() {
+            System.out.println(getClass().getName());
+        }
+
+        @GetMapping("/hello")
+        @ResponseBody
+        public String execute() {
+
+            System.out.println(resolver.getClass().getName());
+            System.out.println(">>>>>>>>>");
+
+            System.out.println(resolver2.getClass().getName());
+            System.out.println(">>>>>>>>>");
+
+            for (String beanDefinitionName : context.getBeanDefinitionNames()) {
+                System.out.println(beanDefinitionName);
+            }
+
+            if (null != context.getParent()) {
+                System.out.println(">>>>> root");
+                for (String definitionName : context.getParent().getBeanDefinitionNames()) {
+                    System.out.println(definitionName);
+                }
+            }
+
+            return getClass().getName();
+        }
+
+    }
 
 
 }
