8b21c801d40cee0ca44b4f20bdae99b9db388238
Ivan <ivan.gontarenko@gmail.com>
2019-12-06 17:08:57 +0200

    upd

--- adminfly/build/src/main/java/org/papaja/adminfly/module/buildin/controller/BuildInController.java
+++ adminfly/build/src/main/java/org/papaja/adminfly/module/buildin/controller/BuildInController.java
@@ -3,12 +3,16 @@ package org.papaja.adminfly.module.buildin.controller;
 import org.papaja.adminfly.commons.mvc.controller.AbstractController;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Random;
+
+import static java.lang.String.format;
+import static java.util.Optional.ofNullable;
 
 @Controller
 @SuppressWarnings({"unused"})
---
+++ adminfly/build/src/main/java/org/papaja/adminfly/module/clock/controller/JustClockController.java
@@ -0,0 +1,35 @@
+package org.papaja.adminfly.module.clock.controller;
+
+import org.papaja.adminfly.commons.mvc.controller.AbstractController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import static java.util.Optional.ofNullable;
+import static org.papaja.adminfly.commons.DataHolder.HOLDER;
+
+@Controller
+@RequestMapping("/shared")
+public class JustClockController extends AbstractController {
+
+    private static final String LOGO_TEXT = "\uD83D\uDD52 Just Clock";
+
+    {
+        setPrefix("clock");
+    }
+
+    @RequestMapping({"/clock", "/clock/{theme:\\w+}"})
+    public ModelAndView index(
+            @PathVariable(name = "theme", required = false) String theme
+    ) {
+        String       name = ofNullable(theme).orElse("default");
+        ModelAndView mav  = newView("index/index");
+
+        HOLDER.set("main.name", LOGO_TEXT);
+        mav.addObject("name", name);
+
+        return mav;
+    }
+
+}
