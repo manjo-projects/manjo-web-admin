1c4cac4e41accba4bf8f7ba73684a8ea3fb74f55
Ivan Hontarenko <ihontarenko@gmail.com>
2020-01-05 13:53:38 +0200

    Make context abstraction for Test App eNV

--- adminfly/build/src/main/java/org/papaja/adminfly/module/psy/tests/PayloadContext.java
+++ adminfly/build/src/main/java/org/papaja/adminfly/module/psy/tests/PayloadContext.java
@@ -4,12 +4,14 @@ import static org.papaja.adminfly.module.psy.tests.Context.UNSUPPORTED_OPERATION
 
 public interface PayloadContext<V> extends PayloadAware<V> {
 
+    String NAME = PayloadContext.class.getCanonicalName();
+
     default Payload getPayload() {
-        throw UNSUPPORTED_OPERATION_EXCEPTION.apply(getClass().getCanonicalName(), "getPayload");
+        throw UNSUPPORTED_OPERATION_EXCEPTION.apply(NAME, "getPayload");
     }
 
     default void setPayload(Payload payload) {
-        throw UNSUPPORTED_OPERATION_EXCEPTION.apply(getClass().getCanonicalName(), "setPayload");
+        throw UNSUPPORTED_OPERATION_EXCEPTION.apply(NAME, "setPayload");
     }
 
 }
--- adminfly/build/src/main/java/org/papaja/adminfly/module/psy/tests/SessionContext.java
+++ adminfly/build/src/main/java/org/papaja/adminfly/module/psy/tests/SessionContext.java
@@ -4,21 +4,23 @@ import static org.papaja.adminfly.module.psy.tests.Context.UNSUPPORTED_OPERATION
 
 public interface SessionContext extends SessionAware, PatientAware {
 
+    String NAME = PayloadContext.class.getCanonicalName();
+
     default void setSession(SessionDetails session) {
-        throw UNSUPPORTED_OPERATION_EXCEPTION.apply(getClass().getCanonicalName(), "setSession");
+        throw UNSUPPORTED_OPERATION_EXCEPTION.apply(NAME, "setSession");
     }
 
     @Override
     default SessionDetails getSession() {
-        throw UNSUPPORTED_OPERATION_EXCEPTION.apply(getClass().getCanonicalName(), "getSession");
+        throw UNSUPPORTED_OPERATION_EXCEPTION.apply(NAME, "getSession");
     }
 
     default void setPatient(PatientDetails patient) {
-        throw UNSUPPORTED_OPERATION_EXCEPTION.apply(getClass().getCanonicalName(), "setPatient");
+        throw UNSUPPORTED_OPERATION_EXCEPTION.apply(NAME, "setPatient");
     }
 
     @Override
     default PatientDetails getPatient() {
-        throw UNSUPPORTED_OPERATION_EXCEPTION.apply(getClass().getCanonicalName(), "getPatient");
+        throw UNSUPPORTED_OPERATION_EXCEPTION.apply(NAME, "getPatient");
     }
 }
